b33b371cc15bee4f759ba870bf06f07886e54f2c
elsen111 <csstudent1111@gmail.com>
2026-09-15 19:50:25 +0300

    feat: project initialized and customer feature added

---
+++ queue-api/src/main/java/com/queueapi/QueueApiApplication.java
@@ -0,0 +1,13 @@
+package com.queueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueApiApplication.class, args);
+    }
+
+}
